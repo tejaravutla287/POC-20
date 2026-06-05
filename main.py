cccbb19608182c07f52f21739451243b672fa9bb
tejaravutla287 <tejaravutla287@gmail.com>
2026-06-05 14:27:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class MyHandler(SimpleHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"<h1>Welcome to the LIVE Python GitOps POC Application! V2</h1>")
+        self.wfile.write(b"Hello from POC-20 Python App 🚀")
 
 if __name__ == "__main__":
-    server = HTTPServer(('0.0.0.0', 8080), MyHandler)
-    print("Python app running on port 8080...")
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    print("Server running on port 8080...")
     server.serve_forever()
